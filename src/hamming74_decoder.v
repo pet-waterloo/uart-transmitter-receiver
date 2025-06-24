02109e50f5f8ebbf2b9d1edb994c503f969df5f4
Peter Zhang <34387360+Ultrasword@users.noreply.github.com>
2025-06-24 15:52:58 -0400

    Update hamming74_decoder.v

--- src/hamming74_decoder.v
+++ src/hamming74_decoder.v
@@ -7,7 +7,6 @@
 
 `default_nettype none
 
-
 module tt_um_hamming_decoder_74 (
     // 1 wire input
     // 4 wire output
@@ -20,8 +19,8 @@ module tt_um_hamming_decoder_74 (
     input wire decode_in,
 
     // 1 byte of output
-    output reg valid_out, // indicates if the output is valid
-    output reg [3:0] decode_out // decoded output
+    output wire valid_out, // indicates if the output is valid
+    output wire [3:0] decode_out // decoded output
 );
     
 
@@ -49,8 +48,8 @@ module tt_um_hamming_decoder_74 (
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n) begin
             // reset logic
-            decode_out <= 0;
-            valid_out <= 0;
+            decode_out_reg <= 0;
+            valid_out_reg <= 0;
         end else if (ena) begin
             // if component is ENABLED
 
@@ -74,13 +73,13 @@ module tt_um_hamming_decoder_74 (
                 end
 
                 // decode the input
-                decode_out[0] <= input_buffer[0];
-                decode_out[1] <= input_buffer[1];
-                decode_out[2] <= input_buffer[2];
-                decode_out[3] <= input_buffer[4];
+                decode_out_reg[0] <= input_buffer[0];
+                decode_out_reg[1] <= input_buffer[1];
+                decode_out_reg[2] <= input_buffer[2];
+                decode_out_reg[3] <= input_buffer[4];
 
                 // set valid output
-                valid_out <= 1;
+                valid_out_reg <= 1;
 
             end else begin
                 // set counter index to be received value
@@ -88,15 +87,14 @@ module tt_um_hamming_decoder_74 (
             end
             
         end else begin
-            valid_out <= 0;
+            valid_out_reg <= 0;
         end
     end
 
     // -------------------------------------------- //
 
-    // Choose one design pattern - using the Hamming decoder:
-    assign uo_out[3:0] = decode_out; // From Hamming decoder
-    assign uo_out[6:4] = 3'b0;       // Middle bits
-    assign uo_out[7] = valid_out;    // Valid bit from decoder
+    // Output assignments
+    assign valid_out = valid_out_reg;
+    assign decode_out = decode_out_reg;
 
 endmodule
